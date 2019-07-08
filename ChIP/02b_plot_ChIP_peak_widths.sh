c307e0f039f9de7318948be83d949efd376b0991
Henriette Miko <henriette.miko@googlemail.com>
2019-07-08 15:52:02 +0200

    Add files via upload

---
+++ ChIP/02b_plot_ChIP_peak_widths.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ../set_guix_profile.sh
+source ../set_variables_hg19.sh
+
+
+QUALITY_DIR=$OUTPUT_DIR/quality_ChIP
+
+mkdir -p $QUALITY_DIR
+cd $QUALITY_DIR
+
+
+#JAMM peaks
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K27ac
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K27me3
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K4me3
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K4me1
+
+
+#MACS2 peaks
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_MACS2_peak_widths.r $OUTPUT_DIR H3K27ac
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_MACS2_peak_widths.r $OUTPUT_DIR H3K27me3
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_MACS2_peak_widths.r $OUTPUT_DIR H3K4me3
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_MACS2_peak_widths.r $OUTPUT_DIR H3K4me1
+
+
+exit
