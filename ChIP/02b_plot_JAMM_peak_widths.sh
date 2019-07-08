de3900193512274d066e5cb88c3a1adf551e0265
Henriette Miko <henriette.miko@googlemail.com>
2019-07-08 17:09:53 +0200

    Add files via upload

---
+++ ChIP/02b_plot_JAMM_peak_widths.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+##########
+#name:          02b_plot_JAMM_peak_widths.sh
+#description:   plot widths of peaks called with JAMM
+#author:        Henriette Miko (henriette.miko@mdc-berlin.de)
+#date:          July 8, 2019
+##########
+
+
+source ../set_guix_profile.sh
+source ../set_variables_hg19.sh
+
+QUALITY_DIR=$OUTPUT_DIR/quality_ChIP
+
+mkdir -p $QUALITY_DIR
+cd $QUALITY_DIR
+
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K27ac
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K27me3
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K4me3
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K4me1
+
+
+exit
+
