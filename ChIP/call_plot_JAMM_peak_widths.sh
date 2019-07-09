65c253a09de6b7cce692fb3acdfb5205a8b86026
Henriette Miko <henriette.miko@googlemail.com>
2019-07-09 11:48:02 +0200

    Add files via upload

---
+++ ChIP/call_plot_JAMM_peak_widths.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##########
+#name:          call_plot_JAMM_peak_widths.sh
+#description:   call plot JAMM peak width Rscripts
+#author:        Henriette Miko (henriette.miko@mdc-berlin.de)
+#date:          July 9, 2019
+##########
+
+
+OUTPUT_DIR=$1
+MARK=$2
+SCRIPT_DIR=$3
+QUALITY_DIR=$4
+
+echo "#####################"
+echo $0 started on `hostname` at `date` with parameters $*
+echo "Started processing:"
+echo "current mark: $MARK"
+echo "#####################"
+
+
+Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR $MARK $QUALITY_DIR
+
+
+exit
+
