6fe9e4dcad8af8130fe8884ae5faa5c1b70bf254
Henriette Miko <henriette.miko@googlemail.com>
2019-07-09 11:47:44 +0200

    Update 02b_plot_JAMM_peak_widths.sh

--- ChIP/02b_plot_JAMM_peak_widths.sh
+++ ChIP/02b_plot_JAMM_peak_widths.sh
@@ -11,14 +11,19 @@
 source ../set_variables_hg19.sh
 
 QUALITY_DIR=$OUTPUT_DIR/quality_ChIP
-
 mkdir -p $QUALITY_DIR
-cd $QUALITY_DIR
 
-Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K27ac
-Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K27me3
-Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K4me3
-Rscript $SCRIPT_DIR/ChIP/plot_ChIP_JAMM_peak_widths.r $OUTPUT_DIR H3K4me1
+for f in $OUTPUT_DIR/ChIP/H3K*
+do
+
+    MARK=$(echo $f | rev | cut -d "/" -f 1 | rev)
+    echo $MARK
+
+    qsub -hold_jid "call_JAMM" -V -j y \
+        -o $QUALITY_DIR/plot_peaks_${MARK}.txt -cwd -pe smp 1 \
+        -l mem_free=2G,h_vmem=2G -l h_rt=24:00:00 \
+        $SCRIPT_DIR/ChIP/call_plot_JAMM_peak_widths.sh $OUTPUT_DIR \
+        $MARK $SCRIPT_DIR $QUALITY_DIR
 
 
 exit
