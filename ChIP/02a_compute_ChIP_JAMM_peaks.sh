18335a749f3efa94a37a703df2768f0a1f1a33d8
Henriette Miko <henriette.miko@googlemail.com>
2019-07-09 15:32:02 +0200

    Update 02a_compute_ChIP_JAMM_peaks.sh

--- ChIP/02a_compute_ChIP_JAMM_peaks.sh
+++ ChIP/02a_compute_ChIP_JAMM_peaks.sh
@@ -26,7 +26,7 @@ do
     mkdir -p $JAMM_DIR
 
     qsub -N call_JAMM_${MARK}_${TIME} \
-        -hold_jid "process_ChIP_fastq" -V -j y \
+        -hold_jid "process_ChIP_fastq_*" -V -j y \
         -o $JAMM_DIR/call_JAMM.txt -cwd -pe smp 1 \
         -l mem_free=30G,h_vmem=30G -l h_rt=24:00:00 \
         $SCRIPT_DIR/ChIP/call_JAMM.sh $JAMM_DIR $MARK $TIME $OUTPUT_DIR \
