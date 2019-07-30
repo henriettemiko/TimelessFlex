10b0287449f56b6e2a88c43c9e679bc2bfd5d2dc
Henriette Miko <henriette.miko@googlemail.com>
2019-07-30 13:55:36 +0200

    Add files via upload

--- open_regions_subset/open_regions_pairs/collapse_time_information.r
+++ open_regions_subset/open_regions_pairs/collapse_time_information.r
@@ -1,13 +1,22 @@
 
-library(plyr)
+##########
+#name:          collapse_time_information.r
+#description:   collapses Hi-C time information for same bins
+#author:        Henriette Miko (henriette.miko@mdc-berlin.de)
+#date:          July 30, 2019
+##########
 
+
+library(plyr)
 
 args = commandArgs(trailingOnly=TRUE)
 
 setwd(getwd())
 
 
-#collapsing time point information for pairs that occur multiple times in file (one line for one time point before, after one line with all time points comma separated)
+#collapsing time point information for pairs that occur multiple times in file
+#before: one line for one time point
+#after: one line with all time points comma separated
 
 t = read.table(args[1])
 numClusters=args[2]
@@ -18,12 +27,18 @@ numClusters=args[2]
 t2=ddply(t, .(V2,V3,V4,V5,V6,V7,V8), summarize, V9summ=paste(V9,collapse=","))
 dim(t2)
 print(head(t2))
-write.table(t2, file=paste0("pairs_", numClusters, "classes_HiC_collapsed.txt"), sep="\t", quote = F, row.names = F, col.names=F)
+write.table(t2, file=paste0("pairs_", numClusters, 
+                            "classes_HiC_collapsed.txt"), sep="\t", quote = F, 
+            row.names = F, col.names=F)
 
-t3=ddply(t, .(V2,V3,V4,V5,V6,V7), summarize,  V8summ=paste(V8,collapse=","), V9summ=paste(V9,collapse=","))
+t3=ddply(t, .(V2,V3,V4,V5,V6,V7), summarize,  V8summ=paste(V8,collapse=","), 
+         V9summ=paste(V9,collapse=","))
 dim(t3)
 print(head(t3))
-write.table(t3, file=paste0("pairs_", numClusters, "classes_HiC_collapsed2.txt"), sep="\t", quote = F, row.names = F, col.names=F)
+write.table(t3, file=paste0("pairs_", numClusters, 
+                            "classes_HiC_collapsed2.txt"), sep="\t", 
+            quote = F, row.names = F, col.names=F)
 
 
 q()
+
